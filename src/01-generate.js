f7e51df4982453d2891c37a9a54a8ece98c94aa5
Luca Del Puppo <luca@delpuppo.net>
2024-02-15 20:06:02 +0100

    remove generated code

--- src/01-generate.js
+++ src/01-generate.js
@@ -1,22 +1,3 @@
 // generate a fastify server and start it
 // it will listen on port 3000
 // and it will expose a single route /hello
-
-const fastify = require('fastify')({
-  logger: true
-})
-
-fastify.get('/hello', async (request, reply) => {
-  return { hello: 'world' }
-})
-
-const start = async () => {
-  try {
-    await fastify.listen(3000)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-
-start()
